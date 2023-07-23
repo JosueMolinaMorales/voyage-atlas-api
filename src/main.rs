ac116e0a34e40d3c1e2f001059bafff90928a0ff
Josue Morales <molinajosue92@hotmail.com>
2023-07-23 10:10:54 -0400

    add skeleton for api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use voyage_atlas_api::api::{get_subscriber, init_subscriber, get_configuration, Application};
+
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+    // Setting up Logging
+    let subscriber = get_subscriber("voyage-atlas-api".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+    Ok(())
 }
